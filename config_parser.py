8bf5528447024405024a453c77d80a9a6414377f
MostHappyCougar <mosthappycougar@gmail.com>
2022-12-17 12:35:36 +0400

    Architecture graph updated

--- config_parser.py
+++ config_parser.py
@@ -4,10 +4,10 @@ conf = ET.parse("config.xml")
 root = conf.getroot()
 
 for tags_conf in root.findall("conf"):
-    height = tags_conf.find("img_h").text
     width = tags_conf.find("img_w").text
-    window_size_h = tags_conf.find("window_size_h").text
+    height = tags_conf.find("img_h").text
     window_size_w = tags_conf.find("window_size_w").text
+    window_size_h = tags_conf.find("window_size_h").text
     source_folder = tags_conf.find("source_folder").text
     output_folder = tags_conf.find("output_folder").text
     
