57ce051dcd4db459a109f9d297964b9598b5da19
MostHappyCougar <mosthappycougar@gmail.com>
2022-12-24 14:43:26 +0400

    Fix issue

--- config_parser.py
+++ config_parser.py
@@ -10,9 +10,9 @@ root = conf.getroot()
 
 for tags_conf in root.findall("conf"):
     width = int(tags_conf.find("img_w").text) if int(tags_conf.find("img_w").text) > 0 else iss_handler("Config Error", "img_w must be greater than zero!", "conf_err01")
-    height = int(tags_conf.find("img_h").text) if int(tags_conf.find("img_h").text) > 0 else iss_handler("img_h must be greater than zero!")
+    height = int(tags_conf.find("img_h").text) if int(tags_conf.find("img_h").text) > 0 else iss_handler("Config Error", "img_h must be greater than zero!", "conf_err01")
     window_size_w = int(tags_conf.find("window_size_w").text) if int(tags_conf.find("window_size_w").text) > 0 else iss_handler("Config Error", "window_size_w must be greater than zero!", "conf_err02")
-    window_size_h = int(tags_conf.find("window_size_h").text) if int(tags_conf.find("window_size_h").text) > 0 else iss_handler("Config Error", "window_size_h must be greater than zero!", "conf_err03")
+    window_size_h = int(tags_conf.find("window_size_h").text) if int(tags_conf.find("window_size_h").text) > 0 else iss_handler("Config Error", "window_size_h must be greater than zero!", "conf_err02")
     
     src_folder_path = path.join(dirname, tags_conf.find("source_folder").text) #Source Folder abs path
     out_folder_path = path.join(dirname, tags_conf.find("output_folder").text) #Output Folder abs path
