dca834fe4b01f59c3234a059db956a74469093f9
MostHappyCougar <mosthappycougar@gmail.com>
2023-01-02 22:28:53 +0400

    Bug Fix

--- hdf5_exporter.py
+++ hdf5_exporter.py
@@ -9,20 +9,18 @@ def expore_image(output_folder, image, coords):
 
     if path.exists(path.join(output_folder, filename)) == True:
         with h5py.File(path.join(output_folder, filename), mode) as hdf5_dataset:
-            train_group = hdf5_dataset.get(dataset)
             
-            try:
+            if f'{dataset}' in hdf5_dataset:
+                train_group = hdf5_dataset.get(dataset)
                 images, coords = train_group.get('Images'), train_group.get('Marks')
                 images.resize(images.shape[0] + 1, axis=0)
                 coords.resize(coords.shape[0] + 1, axis=0)
-            except AttributeError:
+                images[img_arr.shape[0]-1:] = image
+                coords[coords.shape[0]-1:] = coords_arr
+            else:
                 train_group = hdf5_dataset.create_group(dataset)
                 train_group.create_dataset("Images", data=img_arr, chunks=True, maxshape=(None, img_arr.shape[1], img_arr.shape[2], img_arr.shape[3]))
                 train_group.create_dataset("Marks", data=coords_arr, chunks=True, maxshape=(None, coords_arr.shape[1], coords_arr.shape[2]))
-                
-            
-            images[img_arr.shape[0]-1:] = image
-            coords[coords.shape[0]-1:] = coords_arr
     else:
         with h5py.File(path.join(output_folder, filename), 'w') as hdf5_dataset:
             train_group = hdf5_dataset.create_group(dataset)
