cb454bae0faccf48643b372f1d23d859cf42aba1
MostHappyCougar <mosthappycougar@gmail.com>
2023-01-02 19:40:59 +0400

    fix bug

--- hdf5_exporter.py
+++ hdf5_exporter.py
@@ -11,9 +11,14 @@ def expore_image(output_folder, image, coords):
         with h5py.File(path.join(output_folder, filename), mode) as hdf5_dataset:
             train_group = hdf5_dataset.get(dataset)
             
-            images, coords = train_group.get('Images'), train_group.get('Marks')
-            images.resize(images.shape[0] + 1, axis=0)
-            coords.resize(coords.shape[0] + 1, axis=0)
+            try:
+                images, coords = train_group.get('Images'), train_group.get('Marks')
+                images.resize(images.shape[0] + 1, axis=0)
+                coords.resize(coords.shape[0] + 1, axis=0)
+            except AttributeError:
+                train_group.create_dataset("Images", data=img_arr, chunks=True, maxshape=(None, img_arr.shape[1], img_arr.shape[2], img_arr.shape[3]))
+                train_group.create_dataset("Marks", data=coords_arr, chunks=True, maxshape=(None, coords_arr.shape[1], coords_arr.shape[2]))
+                
             
             images[img_arr.shape[0]-1:] = image
             coords[coords.shape[0]-1:] = coords_arr
@@ -22,5 +27,4 @@ def expore_image(output_folder, image, coords):
             train_group = hdf5_dataset.create_group(dataset)
             train_group.create_dataset("Images", data=img_arr, chunks=True, maxshape=(None, img_arr.shape[1], img_arr.shape[2], img_arr.shape[3]))
             train_group.create_dataset("Marks", data=coords_arr, chunks=True, maxshape=(None, coords_arr.shape[1], coords_arr.shape[2]))
-            hdf5_dataset.close()
     
